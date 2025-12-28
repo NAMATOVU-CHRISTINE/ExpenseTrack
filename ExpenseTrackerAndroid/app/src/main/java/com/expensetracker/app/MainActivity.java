105c50f1b3c0cbc2c8b17a9af63b07f22995d5a2
Christine <2023bcs004@std.must.ac.ug>
2025-12-28 08:31:16 +0300

    Fix Material3 theme: add dependencies, use AppCompatActivity, add .gitignore

--- ExpenseTrackerAndroid/app/src/main/java/com/expensetracker/app/MainActivity.java
+++ ExpenseTrackerAndroid/app/src/main/java/com/expensetracker/app/MainActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     
     private WebView webView;
     
