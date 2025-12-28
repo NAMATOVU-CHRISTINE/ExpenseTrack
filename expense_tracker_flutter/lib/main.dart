4bbc833207752bd6804db496697db32131e091b5
Christine <2023bcs004@std.must.ac.ug>
2025-12-28 07:47:33 +0300

    Update app icon

--- expense_tracker_flutter/lib/main.dart
+++ expense_tracker_flutter/lib/main.dart
@@ -704,3 +704,4 @@ class _HomePageState extends State<HomePage> {
 // Update 31 - Update splash screen
 // Update 32 - Fix memory leak
 // Update 33 - Add expense import
+// Update 34 - Update app icon
